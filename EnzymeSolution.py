19dc511b4da90b37a0911573ccf9eb90fc3e20c7
George Montenegro <montenegrogeorge@gmail.com>
2023-02-28 22:08:37 -0800

    Updated filter function

--- EnzymeSolution.py
+++ EnzymeSolution.py
@@ -20,34 +20,38 @@ class EnzymeSolution:
     unionsq_coor = (37.788037692341184, -122.4073609530195)
     fac_type = "Truck"
     japanese_food = ["japan", "poke"]
-    filters = ['facilitytype', 'fooditems', 'latitude', 'longitude']
+    filters = ['facilitytype', 'fooditems', 'latitude', 'longitude', 'applicant', 'address']
+
+    japanese_trucks = []
 
     # FN Get JSON from website
-    def get_json(self):
-        try:
-            with urllib.request.urlopen(self.json_url) as url:
-                json_data = json.load(url)
+    def get_json(self, json_url=None):
+        if json_url is None:
+            json_url = self.json_url
 
-            return json_data
+        try:
+            with urllib.request.urlopen(json_url) as url:
+                json_data = json.load(url)
+                self.japanese_trucks = json_data
         except ValueError:
             print("Error Pulling JSON from URL")
 
     # Clean JSON of incomplete data
     def clean_json(self, json_data=None):
         if json_data is None:
-            json_data = self.get_json()
+            json_data = self.japanese_trucks
 
         for j in self.filters:
             for i in json_data:
                 if j not in i:
                     json_data.remove(i)
 
-        return json_data
+        self.japanese_trucks = json_data
 
     # FN to Filter List of Objects
     def filter_json(self, json_data=None):
         if json_data is None:
-            json_data = self.clean_json()
+            json_data = self.japanese_trucks
 
         data = []
 
@@ -57,27 +61,36 @@ class EnzymeSolution:
                     if re.search(j, entry['fooditems'].lower()):
                         data.append(entry)
 
-        return data
+        self.japanese_trucks = data
 
     # create tuples of coordinates
     def create_coor(self, json_data=None):
         if json_data is None:
-            json_data = self.filter_json()
-
-        coors = {}
+            json_data = self.japanese_trucks
 
         for i in json_data:
-            coors[i['applicant'] + ' - ' + i['address']] = geopy.distance.geodesic((i['latitude'], i['longitude']),
-                                                                                   self.unionsq_coor).miles
+            i['distance'] = geopy.distance.geodesic((i['latitude'], i['longitude']), self.unionsq_coor).miles
 
-        return coors
+        self.japanese_trucks = json_data
 
-    # FN Calculate distance to Union Square
-    def closest_to_union(self, distances=None):
+    def sort_list(self, json_data=None):
+        if json_data is None:
+            json_data = self.japanese_trucks
+
+        self.japanese_trucks = sorted(json_data, key=lambda d: d['distance'])
+
+    # # FN Calculate distance to Union Square
+    def closest_to_union(self, url=None, distances=None):
+        self.get_json(url)
+        self.clean_json(distances)
+        self.filter_json(self.clean_json(distances))
+        self.create_coor(self.filter_json(self.clean_json(distances)))
+        self.sort_list(self.create_coor(self.filter_json(self.clean_json(distances))))
+
         if distances is None:
-            distances = self.create_coor()
+            distances = self.japanese_trucks
 
-        return min(distances, key=distances.get)
+        return distances[0]['applicant'] + ' at ' + distances[0]['address'] + ' with a distance of ' + str(distances[0]['distance']) + ' miles'
 
     def __init__(self):
         pass
